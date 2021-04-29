08c93499f9280186f6915d2c7e06dfed82c5aaff
Aditya Bakshi <81141123+AdityaBakshi5@users.noreply.github.com>
2021-04-29 14:51:54 +0530

    Update main.c

--- main.c
+++ main.c
@@ -2,6 +2,9 @@
 #include <util/delay.h>
 #include "../inc/Activity1.h"
 #include "../inc/Activity2.h"
+
+#define F_CPU 16000000UL 	/**< Clock Frequency of MCU is 16 MHz */
+
 int main()
 {
     // Calling the system_init function which determines the status of LED actuator
