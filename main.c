f2408d1b8a6c2d00110bee1d758b90e3965b3828
Aditya Bakshi <81141123+AdityaBakshi5@users.noreply.github.com>
2021-04-29 10:58:21 +0530

    Update main.c

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <avr/io.h>
 #include <util/delay.h>
-#include "inc/Activity1.h"
-#include "inc/Activity2.h"
+#include "../inc/Activity1.h"
+#include "../inc/Activity2.h"
 int main()
 {
     // Calling the system_init function which determines the status of LED actuator
