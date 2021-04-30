100d165c599333029d0e64789ac374be3923157b
Aditya Bakshi <81141123+AdityaBakshi5@users.noreply.github.com>
2021-04-30 19:05:15 +0530

    Update Activity3.c

--- src/Activity3.c
+++ src/Activity3.c
@@ -5,7 +5,8 @@
 #include "Activity4.h"
 // temp1 contains the temperature value that ADC renders
 unsigned volatile temp1;
-void Timer()
+
+int activity3()
 {
     // using 16-bit timer and counter1
     TCCR1A|=(1<<COM1A1)|(1<<WGM11)|(1<<WGM10);
@@ -13,12 +14,10 @@ void Timer()
     TCCR1B|=(1<<WGM12)|(1<<CS11)|(1<<CS10); 
     // Setting direction of PB1 hardware pin as output
     DDRB|=(1<<PB1); 
-}
-int activity3()
-{
-    Timer();
+    
     // OCR1A (16-bit register contains the converted value which is held by ADCregister)
     OCR1A=ADCregister;
+    
     // conditions for temperature as per the % PWM given
     if (ADCregister>=0 && ADCregister<=200)
     {
